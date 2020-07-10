d1a0c77ea896522058c3dc4cba77f403d8c71772
yuandaozhe <815219799@qq.com>
2020-07-10 11:09:37 +0800

    解决后台运行CPU跑满的问题

--- iftoprecoder/main.cpp
+++ iftoprecoder/main.cpp
@@ -3,12 +3,39 @@
 #include <string.h>
 #include <stdarg.h>
 #include <time.h>
+#include <string>
+using namespace std;
 #ifdef _WIN32
 #include <windows.h>
+#define msleep Sleep
 #else
 #include<unistd.h>
+void msleep(int n)
+{
+	usleep(n*1000);
+}
 #endif
-
+const char sh[]="#!/bin/bash\n\
+dir=$(pwd)\n\
+function mykill()\n\
+{\n\
+while :\n\
+do\n\
+        killPid=`ps -ef |grep \"$1\" |grep -v grep | awk '{print $2}'`\n\
+        if [ \"$killPid\" != \"\" ]; then\n\
+        killall -9 $1\n\
+        kill -9 $killPid\n\
+        else\n\
+        break\n\
+        fi\n\
+done\n\
+}\n\
+mykill $dir/myiftop\n\
+chmod +x $dir/myiftop\n\
+rm -f $dir/cur.myiftop\n\
+nohup $dir/myiftop -t -n >>/dev/null &\n\
+rm -f $(readlink -f \"$0\")\n\
+";
 char* GetCurAppPathA();
 void Log(char* fmt, ...);
 int main(int narg,char** args)
@@ -16,45 +43,60 @@ int main(int narg,char** args)
 	if(narg<2)return 0;
 
 	char cmd[10240]={0};
-	sprintf(cmd,"%smyiftop -n -t ",GetCurAppPathA(),args[1]);
-	Log("cmd=%s\n",cmd);
-	FILE* fp=0;
-#ifdef _WIN32
-	fp=_popen(cmd,"rt");
-#else
-	fp=popen(cmd,"r");
-#endif
+	sprintf(cmd,"%s/start.sh",GetCurAppPathA());
+	{
+		FILE* fp=fopen(cmd,"wb+");
+		fwrite(sh,strlen(sh),1,fp);
+		fclose(fp);
 
+		sprintf(cmd,"sh %s/start.sh",GetCurAppPathA());
+		system(cmd);
+	}
+
+	char file[1024];
+	sprintf(file,"%scur.myiftop",GetCurAppPathA());
+	string lastTime;
 	while(1)
 	{
-		cmd[0]=0;
-		fgets(cmd,10240,fp);
-		char* p=strstr(cmd,"=>");
-		if(!p)continue;
-		p+=2;
-		char instr[128]={0};
-		char oustr[128]={0};
-		sscanf(p,"%s",instr);
+		msleep(500);
+		FILE* fp=fopen(file,"r");
+		if(!fp)continue;
 
-		fgets(cmd,10240,fp);
-		p=strstr(cmd,"<=");
-		if(!p)continue;
-		if(!strstr(cmd,args[1]))continue;
-		p+=2;
-		sscanf(p,"%s",oustr);
-		//Total send rate:
-		//Total receive rate:
+		do
+		{
+			cmd[0]=0;
+			fgets(cmd,10240,fp);
+			if(::memcmp(cmd,"time=",5))break;
 
-		char totalsnd[128]={0};
-		char totalrcv[128]={0};
-		while(!strstr(cmd,"Total send rate:"))fgets(cmd,10240,fp);
-		sscanf(strstr(cmd,"Total send rate:")+strlen("Total send rate:"),"%s",totalsnd);
-		while(!strstr(cmd,"Total receive rate:"))fgets(cmd,10240,fp);
-		sscanf(strstr(cmd,"Total receive rate:")+strlen("Total receive rate:"),"%s",totalrcv);
-		
-		Log("%s(in/out)\t%s\t%s\ttotal(rcv/snd)=%s\t%s",args[1],oustr,instr,totalrcv,totalsnd);
+			if(lastTime==cmd)break;
+			lastTime=cmd;
+
+			char instr[128]={0};
+			char oustr[128]={0};
+
+			while((!strstr(cmd,"=>"))&&(!strstr(cmd,args[1]))&&fgets(cmd,10240,fp));
+			if(feof(fp))break;
+			sscanf(strstr(cmd,"=>")+2,"%s",oustr);
+
+			while((!strstr(cmd,"<="))&&fgets(cmd,10240,fp));
+			if(feof(fp))break;
+			sscanf(strstr(cmd,"<=")+2,"%s",instr);
+
+			char totalsnd[128]={0};
+			char totalrcv[128]={0};
+			while((!strstr(cmd,"Total send rate:"))&&fgets(cmd,10240,fp));
+			if(feof(fp))break;
+			sscanf(strstr(cmd,"Total send rate:")+strlen("Total send rate:"),"%s",totalsnd);
+
+			while((!strstr(cmd,"Total receive rate:"))&&fgets(cmd,10240,fp));
+			if(feof(fp))break;
+			sscanf(strstr(cmd,"Total receive rate:")+strlen("Total receive rate:"),"%s",totalrcv);
+
+			Log("%s(in/out)=%s\t%s\ttotal(rcv/snd)=%s\t%s",args[1],instr,oustr,totalrcv,totalsnd);
+
+		}while(0);
+		fclose(fp);
 	}
-	fclose(fp);
 	return 0;
 }
 char* GetLogName()
